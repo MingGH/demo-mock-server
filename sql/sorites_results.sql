56f3c829351dd3d5f84cddb53cf0e5906c2a0d72
MingGH <1558772318@qq.com>
2026-05-30 16:25:07 +0800

    chore: 删除sql目录(生产数据库导出,已由temp/backup-data替代)

--- sql/sorites_results.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS sorites_results (
-    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
-    sand_boundary   INT          NOT NULL,
-    sand_sharpness  VARCHAR(20)  NOT NULL,
-    bald_boundary   INT          NOT NULL,
-    color_boundary  INT          NOT NULL,
-    created_at      BIGINT       NOT NULL,
-    INDEX idx_created (created_at)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
